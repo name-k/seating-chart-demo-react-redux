bff7fde8b1e9922012022fbf2eea6054da36bbbf
name-k <psykeonfarm@gmail.com>
2016-04-28 02:46:24 +0530

    redux devtools via chrome plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducers from 'reducers';
 import App from './app';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+let store = createStore(reducers, compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>, document.querySelector('[data-app-container]'));
